d3e3afcf7dd12a2e84f8ae3b698049fbc1b6fb51
Faycall1l <fayam69420@gmail.com>
2024-04-27 02:43:38 +0100

    routing to best quay

--- port_simulation/urls.py
+++ port_simulation/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("ship_quay_api.urls")),  # Include the app's URLs
+    path("api/", include("ship_quay_api.urls")),
 ]
